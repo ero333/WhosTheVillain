46223c24375a3f88776067c3d843d52b8b505e2f
Byunxxu <priscila.caceresranieri@gmail.com>
2024-09-03 18:01:38 -0300

    +Aregué los scripts para el funcionamiento de la barra de progreso

--- Assets/Code/BarraVillano.cs
+++ Assets/Code/BarraVillano.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class BarraVillano : MonoBehaviour
 {
+    public Image progressBar;
+    public float fillSpeed = 0.5f;
+    public bool isFilling = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,27 @@ public class BarraVillano : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isFilling)
+        {
+            progressBar.fillAmount += fillSpeed * Time.deltaTime;
+            if (progressBar.fillAmount >= 1 ) 
+            {
+                progressBar.fillAmount = 0;
+                isFilling = false;
+                //Destroy(gameObject);
+            }
+        }
+       
+    }
+
+    public void StartFilling()
+    {
+        isFilling = true;
+
+    }
+
+    public void StopFilling()
+    { 
+        isFilling = false; 
     }
 }
---
+++ Assets/Code/ObjetoBarra.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjetoBarra : MonoBehaviour
+{
+    public BarraVillano barravillano;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (barravillano != null) 
+        {
+            barravillano.StartFilling();
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnMouseUp()
+    {
+        if(barravillano != null)
+        {
+            barravillano.StopFilling();
+        }
+    }
+}
