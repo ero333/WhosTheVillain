bb423fdf2ae5ac3a6f8926a442a87d2b180a22dd
Byunxxu <priscila.caceresranieri@gmail.com>
2024-09-04 19:47:41 -0300

    +Agregué el código para que el enemigo entre y salga en determinado tiempo.

--- Assets/Code/EnemigoVillano.cs
+++ Assets/Code/EnemigoVillano.cs
@@ -5,15 +5,38 @@ using UnityEngine.UI;
 
 public class EnemigoVillano : MonoBehaviour
 {
+    public float Aparicion;
+    public float Vigilar;
+    public float cronometro;
+    public Animator anim;
+    public bool Entrada;
+    public bool Salida;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Entrada = true;
+        cronometro = Vigilar;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        cronometro -= Time.deltaTime;
+        if (cronometro <= 0 && Entrada)
+        {
+            Entrada = false;
+            Salida = true;
+            cronometro = Aparicion;
+            anim.SetBool("EnemigoSeVa",true);
+        }
+        else if (cronometro <= 0 && Salida)
+        {
+            Entrada = true;
+            Salida = false;
+            cronometro = Vigilar;
+            anim.SetBool("EnemigoSeVa", false);
+        }
     }
 }
