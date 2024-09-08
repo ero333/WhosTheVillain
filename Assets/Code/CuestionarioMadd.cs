8a24df235b3263ec546b62b3c179685f5d5bef2e
JuanFerrea <juani.ferrea04@gmail.com>
2024-09-08 17:15:55 -0300

    +Agregue el sistema de interrogatorio a la campaña del villano.

---
+++ Assets/Code/CuestionarioMadd.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Cuestionario : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pregunta
+    {
+        public string textoPregunta;
+        public string[] respuestas;
+        public int respuestaCorrecta;
+    }
+
+    public Pregunta[] preguntas;
+    public Text textoPregunta;
+    public Button botonSiguiente;
+    public Button[] botonesRespuestas;
+    public GameObject preguntaPanel;
+    public GameObject respuestaPanel;
+
+    private int preguntaActual = 0;
+    private int respuestasCorrectas = 0;
+
+    void Start()
+    {
+        MostrarPregunta();
+        botonSiguiente.onClick.AddListener(MostrarRespuestas);
+    }
+
+    void MostrarPregunta()
+    {
+        preguntaPanel.SetActive(true);
+        respuestaPanel.SetActive(false);
+        textoPregunta.text = preguntas[preguntaActual].textoPregunta;
+    }
+
+    void MostrarRespuestas()
+    {
+        preguntaPanel.SetActive(false);
+        respuestaPanel.SetActive(true);
+        for (int i = 0; i < botonesRespuestas.Length; i++)
+        {
+            botonesRespuestas[i].GetComponentInChildren<Text>().text = preguntas[preguntaActual].respuestas[i];
+            botonesRespuestas[i].onClick.RemoveAllListeners();
+            int index = i;
+            botonesRespuestas[i].onClick.AddListener(() => VerificarRespuesta(index));
+        }
+    }
+
+    void VerificarRespuesta(int index)
+    {
+        if (index == preguntas[preguntaActual].respuestaCorrecta)
+        {
+            respuestasCorrectas++;
+        }
+
+        preguntaActual++;
+        if (preguntaActual < preguntas.Length)
+        {
+            MostrarPregunta();
+        }
+        else
+        {
+            EvaluarResultado();
+        }
+    }
+
+    void EvaluarResultado()
+    {
+        if (respuestasCorrectas == preguntas.Length)
+        {
+            SceneManager.LoadScene("Pantalla Victoria Villano");
+        }
+        else
+        {
+            SceneManager.LoadScene("Pantalla Derrota Villano");
+        }
+    }
+}
+
+
+
