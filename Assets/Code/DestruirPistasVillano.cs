70c776a3b419cfc15908a68eb5fbfca7299eccf1
JuanFerrea <juani.ferrea04@gmail.com>
2024-09-28 11:55:12 -0300

    +Agregue un contador de pistas destruidas para la campaña del villano.

--- Assets/Code/DestruirPistasVillano.cs
+++ Assets/Code/DestruirPistasVillano.cs
@@ -8,20 +8,31 @@ public class DestruirPistasVillano : MonoBehaviour
     public static int objectsDestroyed = 0;
     public int totalObjects = 5;
     public GameObject button;
+    public Text objectCounterText;
 
     // Start is called before the first frame update
     void Start()
     {
         objectsDestroyed = 0;
         button.SetActive(false);
+        UpdateObjectCounterText();
     }
 
     void OnDestroy()
     {
         objectsDestroyed++;
+        UpdateObjectCounterText();
         if (objectsDestroyed >= totalObjects)
         {
             button.SetActive(true);
+        }
+    }
+
+    void UpdateObjectCounterText()
+    {
+        if (objectCounterText != null)
+        {
+          objectCounterText.text = objectsDestroyed + "/" + totalObjects;
         }
     }
 
