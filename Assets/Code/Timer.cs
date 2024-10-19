36f1101e2cc4a41391771e11402421bbce860a8b
JuanFerrea <juani.ferrea04@gmail.com>
2024-10-19 17:43:53 -0300

    +Agregue el dub log de TimeClue.

---
+++ Assets/Code/Timer.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float timeElapsed; // Tiempo acumulado
+    public Text timerText; // Referencia al componente Text
+
+    void Start()
+    {
+        timeElapsed = 0f; // Inicializa el contador
+    }
+
+    void Update()
+    {
+        timeElapsed += Time.deltaTime; // Aumenta el tiempo cada frame
+        UpdateTimerDisplay(); // Actualiza la visualización del tiempo
+    }
+
+    void UpdateTimerDisplay()
+    {
+        // Formatea el tiempo a minutos y segundos
+        float minutes = Mathf.FloorToInt(timeElapsed / 60);
+        float seconds = Mathf.FloorToInt(timeElapsed % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    void OnDisable()
+    {
+        // Llama a Debug.Log al cambiar de escena
+        Debug.Log("timeclue: Cambiando de escena. Tiempo transcurrido: " + timeElapsed + " segundos.");
+    }
+}
