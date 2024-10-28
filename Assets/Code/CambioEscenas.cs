864c47c40ad3099b2e8281aae93cc4c9b440a367
JuanFerrea <juani.ferrea04@gmail.com>
2024-10-27 22:21:14 -0300

    +Realice algunos cambios en el scritp del cambio de escenas para solucionar el error de itchio.

--- Assets/Code/CambioEscenas.cs
+++ Assets/Code/CambioEscenas.cs
@@ -83,118 +83,141 @@ public class CambioEscenas : MonoBehaviour
 
     public void CambiarEscena(int a)
     {
-        SceneManager.LoadScene(a);
-
+        StartCoroutine(LoadSceneAndCleanUp(a));
     }
 
     public void EventoSkip(int a)
     {
-        SceneManager.LoadScene(a);
+        StartCoroutine(LoadSceneAndCleanUp(a));
         Debug.Log("Skip");
     }
 
     public void SiguienteNivel()
     {
-        SceneManager.LoadScene(nivelActual.buildIndex + 1);
+        StartCoroutine(LoadSceneAndCleanUp(nivelActual.buildIndex + 1));
     }
 
     public void Menu()
     {
-        SceneManager.LoadScene("MenuPrincipal");
+        StartCoroutine(LoadSceneAndCleanUp("MenuPrincipal"));
     }
+
     public void Selector()
     {
-        SceneManager.LoadScene("Selector de niveles");
+        StartCoroutine(LoadSceneAndCleanUp("Selector de niveles"));
     }
 
     public void DNivel1()
     {
-        SceneManager.LoadScene("Nivel 1 Oficina");
+        StartCoroutine(LoadSceneAndCleanUp("Nivel 1 Oficina"));
     }
 
     public void DNivel2()
     {
-        SceneManager.LoadScene("Nivel 2 Oficina D");
+        StartCoroutine(LoadSceneAndCleanUp("Nivel 2 Oficina D"));
     }
 
     public void DNivel3()
     {
-        SceneManager.LoadScene("Nivel 3 Oficina D");
+        StartCoroutine(LoadSceneAndCleanUp("Nivel 3 Oficina D"));
     }
 
     public void CineV1()
     {
-        SceneManager.LoadScene("Cutscene intro villano");
+        StartCoroutine(LoadSceneAndCleanUp("Cutscene intro villano"));
     }
-
 
     public void VNivel1()
     {
-        SceneManager.LoadScene("Nivel 1 villano");
+        StartCoroutine(LoadSceneAndCleanUp("Nivel 1 villano"));
     }
 
     public void CineV2()
     {
-        SceneManager.LoadScene("Cutscene intro villano2");
+        StartCoroutine(LoadSceneAndCleanUp("Cutscene intro villano2"));
     }
 
     public void VNivel2()
     {
-        SceneManager.LoadScene("Nivel 2 villano");
+        StartCoroutine(LoadSceneAndCleanUp("Nivel 2 villano"));
     }
 
     public void VNivel3()
     {
-        SceneManager.LoadScene("Nivel 3 villano");
+        StartCoroutine(LoadSceneAndCleanUp("Nivel 3 villano"));
     }
 
     public void LogrosDetective()
     {
-        SceneManager.LoadScene("Logros Detective");
+        StartCoroutine(LoadSceneAndCleanUp("Logros Detective"));
     }
 
     public void LogrosVillano()
     {
-        SceneManager.LoadScene("Logros Villano");
+        StartCoroutine(LoadSceneAndCleanUp("Logros Villano"));
     }
 
     public void PantallaDerrotaVillano()
     {
-        SceneManager.LoadScene("Pantalla Derrota Villano");
+        StartCoroutine(LoadSceneAndCleanUp("Pantalla Derrota Villano"));
     }
 
     public void VNivel1pt2()
     {
-        SceneManager.LoadScene("Nivel 1 Villano pt2");
+        StartCoroutine(LoadSceneAndCleanUp("Nivel 1 Villano pt2"));
     }
 
     public void VNivel1Interrogatorio()
     {
-        SceneManager.LoadScene("Nivel 1 Villano Interrogatorio");
+        StartCoroutine(LoadSceneAndCleanUp("Nivel 1 Villano Interrogatorio"));
     }
 
     public void PantallaDerrotaDetective()
     {
-        SceneManager.LoadScene("Pantalla Derrota");
+        StartCoroutine(LoadSceneAndCleanUp("Pantalla Derrota"));
     }
 
     public void PantallaVictoriaDetective()
     {
-        SceneManager.LoadScene("Pantalla Victoria");
+        StartCoroutine(LoadSceneAndCleanUp("Pantalla Victoria"));
     }
 
     public void CutsceneDerrotaDetective()
     {
-        SceneManager.LoadScene("Cutscene Derrota D");
+        StartCoroutine(LoadSceneAndCleanUp("Cutscene Derrota D"));
     }
 
     public void C1D()
     {
-        SceneManager.LoadScene("Cutscene Intro Detective");
+        StartCoroutine(LoadSceneAndCleanUp("Cutscene Intro Detective"));
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         SiguienteNivel();
+    }
+
+    private IEnumerator LoadSceneAndCleanUp(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Liberar la escena anterior
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+
+    private IEnumerator LoadSceneAndCleanUp(int sceneIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Liberar la escena anterior
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().name);
     }
 }
