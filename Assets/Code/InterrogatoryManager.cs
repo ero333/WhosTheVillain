9a7e473aa881655bac140e77d9628be03de41b18
JuanFerrea <juani.ferrea04@gmail.com>
2024-11-02 11:55:41 -0300

    +Cambie el script del interrogatorio para ajustarlo a la nueva idea planteada en clase.

--- Assets/Code/InterrogatoryManager.cs
+++ Assets/Code/InterrogatoryManager.cs
@@ -15,38 +15,29 @@ public class InterrogatoryManager : MonoBehaviour
     public Image[] imagenesPersonajes; // Las imágenes para cada personaje
     public GameObject BotonesSospechos; // Los botones para los sospechosos
     public GameObject BotonCambioEscena;
+    public Button BotonSiguiente;
 
     private Personaje personajeActual;
-    public int preguntasRestantes = 3;
-    public int entrevistasRequeridas = 3;
-    public int Entrevistas;
-
-    //public Desbloqueador desbloqueador; // Referencia al Desbloqueador
 
     void Start()
     {
         // Configura el estado inicial del juego
         imagenCajaPreguntas.gameObject.SetActive(false);
+        BotonCambioEscena.SetActive(true); // El botón de cambiar de escena está activado desde el principio
+
+        if (BotonSiguiente != null)
+        {
+            BotonSiguiente.onClick.AddListener(VolverASospechos);
+        }
     }
 
-    private void Update()
-    {
-        TerminarEntrevista();
-    }
-
-    public void SeleccionarPersonaje(int personaje)
+    public void SeleccionarPersonaje(int personajeIndex)
     {
         DesactivarBotones();
 
-        personajeActual = personajes[personaje];
-        preguntasRestantes = 3;
+        personajeActual = personajes[personajeIndex];
         MostrarImagenPersonaje(personajeActual);
         MostrarCajaPreguntas();
-
-        for (int i = 0; i < botonesPreguntas.Length; i++)
-        {
-            botonesPreguntas[i].GetComponent<Button>().interactable = true;
-        }
     }
 
     public void ActivarRespuesta(int preguntaIndex)
@@ -59,97 +50,71 @@ public class InterrogatoryManager : MonoBehaviour
 
         Debug.Log("Question: " + preguntaIndex);
 
-        foreach (Personaje x in personajes)
+        CajaRespuesta.SetActive(true);
+        NombrePersonaje.text = personajeActual.nombre;
+        RespuestaPersonaje.text = personajeActual.preguntas[preguntaIndex].respuesta;
+
+        // Verificar si la respuesta es correcta
+        if (personajeActual.preguntas[preguntaIndex].esCorrecta)
         {
-            if (personajeActual == x)
-            {
-                CajaRespuesta.SetActive(true);
-                NombrePersonaje.text = x.nombre;
-                RespuestaPersonaje.text = x.preguntas[preguntaIndex].respuesta;
-
-                DesactivarBotonPregunta(preguntaIndex);
-
-                // Verificar si la respuesta es correcta
-                if (x.preguntas[preguntaIndex].esCorrecta)
-                {
-                    x.CorrectAnswers++;
-                    Debug.Log("Respuesta correcta para " + x.nombre);
-
-                    // Desbloquear logro si se cumplen las condiciones
-                   
-                }
-                else
-                {
-                    x.IncorrectAnswers++;
-                    Debug.Log("Respuesta incorrecta para " + x.nombre);
-                }
-            }
+            personajeActual.CorrectAnswers++;
+            Debug.Log("Respuesta correcta para " + personajeActual.nombre);
         }
-        preguntasRestantes--;
+        else
+        {
+            personajeActual.IncorrectAnswers++;
+            Debug.Log("Respuesta incorrecta para " + personajeActual.nombre);
+        }
+
+        // Volver a la selección de sospechosos después de mostrar la respuesta
+        BotonSiguiente.onClick.RemoveAllListeners();
+        BotonSiguiente.onClick.AddListener(VolverASospechos);
     }
 
     private void DesactivarBotones()
     {
         BotonesSospechos.SetActive(false);
+        BotonCambioEscena.SetActive(false); // Desactivar el botón de cambiar de escena
     }
 
     private void MostrarImagenPersonaje(Personaje personaje)
     {
-        if (preguntasRestantes > 0)
+        for (int i = 0; i < imagenesPersonajes.Length; i++)
         {
-            for (int i = 0; i < imagenesPersonajes.Length; i++)
-            {
-                imagenesPersonajes[i].gameObject.SetActive(personajes[i] == personaje);
-            }
-        }
-        else
-        {
-            imagenCajaPreguntas.gameObject.SetActive(false);
-            for (int i = 0; i < imagenesPersonajes.Length; i++)
-            {
-                if (imagenesPersonajes[i].gameObject.activeSelf)
-                    imagenesPersonajes[i].gameObject.SetActive(false);
-            }
+            imagenesPersonajes[i].gameObject.SetActive(personajes[i] == personaje);
         }
     }
 
     public void MostrarCajaPreguntas()
     {
-        if (preguntasRestantes > 0)
+        imagenCajaPreguntas.gameObject.SetActive(true);
+        for (int i = 0; i < botonesPreguntas.Length; i++)
         {
-            imagenCajaPreguntas.gameObject.SetActive(true);
-            for (int i = 0; i < botonesPreguntas.Length; i++)
-            {
-                botonesPreguntas[i].gameObject.SetActive(true);
-            }
-        }
-        else
-        {
-            Entrevistas++;
-            MostrarImagenPersonaje(personajeActual);
-            personajeActual = null;
-            BotonesSospechos.SetActive(true);
+            botonesPreguntas[i].gameObject.SetActive(true);
         }
     }
 
-    private void DesactivarBotonPregunta(int preguntaIndex)
+    public void VolverASospechos()
     {
-        // Desactiva el botón de la pregunta correspondiente
-        botonesPreguntas[preguntaIndex].GetComponent<Button>().interactable = false;
-    }
-
-    private void TerminarEntrevista()
-    {
-        if (Entrevistas == entrevistasRequeridas)
+        for (int i = 0; i < imagenesPersonajes.Length; i++)
         {
-            BotonCambioEscena.SetActive(true);
+            imagenesPersonajes[i].gameObject.SetActive(false);
         }
+        imagenCajaPreguntas.gameObject.SetActive(false);
+        CajaRespuesta.SetActive(false);
+
+        // Reactivar todos los botones de sospechosos
+        ReactivarBotonesSospechos();
+
+        BotonesSospechos.SetActive(true);
+        BotonCambioEscena.SetActive(true); // Reactivar el botón de cambiar de escena
     }
 
-    public void PersonajeSeleccionado(Button PersonajeBoton)
+    private void ReactivarBotonesSospechos()
     {
-        PersonajeBoton.GetComponent<Button>().interactable = false;
+        for (int i = 0; i < BotonesSospechos.transform.childCount; i++)
+        {
+            BotonesSospechos.transform.GetChild(i).GetComponent<Button>().interactable = true;
+        }
     }
 }
-
-
